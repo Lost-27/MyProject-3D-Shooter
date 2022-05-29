d1c46d9e1c6411be0f0531c6f28fd0729348ad63
Lost-27 <win4ester92@mail.ru>
2022-05-29 16:17:10 +0300

    Managers and Pathfinding. Add AlienBetle logic. Setting NavMesh

---
+++ Assets/Scripts/Game/Enemy/AlienBeetle.cs
@@ -0,0 +1,30 @@
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienBeetle : MonoBehaviour
+{
+    public Transform _target;
+
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private float navigationUpdate;
+    
+    private float _navigationTime = 0;
+
+
+    private void Update()
+    {
+        _navigationTime += Time.deltaTime;
+        if (_navigationTime > navigationUpdate)
+        {
+            _agent.destination = _target.position;
+            _navigationTime = 0;
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,11 +17,57 @@ public class GameManager : MonoBehaviour
     private float _generatedSpawnTime = 0;
     private float _currentSpawnTime = 0;
 
-    private void Start()
-    {
-    }
-
     private void Update()
     {
+        _currentSpawnTime += Time.deltaTime;
+
+        if (_currentSpawnTime > _generatedSpawnTime)
+        {
+            _currentSpawnTime = 0;
+            _generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+
+            if (aliensPerSpawn > 0 && _aliensOnScreen < totalAliens)
+            {
+                List<int> previousSpawnLocations = new List<int>();
+
+                if (aliensPerSpawn > _spawnPoints.Length)
+                {
+                    aliensPerSpawn = _spawnPoints.Length - 1;
+                }
+
+                aliensPerSpawn = (aliensPerSpawn > totalAliens) ? aliensPerSpawn - totalAliens : aliensPerSpawn;
+
+                for (int i = 0; i < aliensPerSpawn; i++)
+                {
+                    if (_aliensOnScreen < maxAliensOnScreen)
+                    {
+                        _aliensOnScreen += 1;
+                        // 1
+                        int spawnPoint = -1;
+                        // 2
+                        while (spawnPoint == -1)
+                        {
+                            // 3
+                            int randomNumber = Random.Range(0, _spawnPoints.Length - 1);
+                            // 4
+                            if (!previousSpawnLocations.Contains(randomNumber))
+                            {
+                                previousSpawnLocations.Add(randomNumber);
+                                spawnPoint = randomNumber;
+                            }
+                        }
+
+                        GameObject spawnLocation = _spawnPoints[spawnPoint];
+                        GameObject newAlienBeetle = Instantiate(_alienBeetle);
+                        newAlienBeetle.transform.position = spawnLocation.transform.position;
+                        AlienBeetle alienBeetle = newAlienBeetle.GetComponent<AlienBeetle>();
+                        alienBeetle._target = _player.transform;
+                        Vector3 targetRotation = new Vector3(_player.transform.position.x,
+                            newAlienBeetle.transform.position.y, _player.transform.position.z);
+                        newAlienBeetle.transform.LookAt(targetRotation);
+                    }
+                }
+            }
+        }
     }
 }
